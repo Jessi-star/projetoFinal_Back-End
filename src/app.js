4c55aa930159a3a714ed34e1e0c41bc19d1e3b8d
Crislayne Santos <crislaynesantos1603@gmail.com>
2024-08-19 14:19:56 -0300

    atualizacao-clientes2

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const router = require('./routers/router');
 const routerCaracteristicaCarro = require('./routers/router-caracteristica-carro');
 const routerUsers = require('./routers/router-users');
 const routerPagamentos= require('./routers/router-pagamentos');
+const routerClients= require('./routers/router-clientes');
+
 
 const app = express();
 app.use(express.json());
@@ -10,8 +12,9 @@ app.use(express.json());
 app.use(router);
 app.use(routerUsers);
 app.use(routerPagamentos);
-
 app.use(routerCaracteristicaCarro);
+app.use(routerClients);
+
 
 
 module.exports = app;
